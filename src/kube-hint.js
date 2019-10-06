578e90dc3ce781da69911ccdf51ecb873d9f771f
erulabs <seandon.mooy@gmail.com>
2019-10-06 11:54:18 -0700

    rewrite summary system a bit, move helpers to KubeHintResults class

--- src/kube-hint.js
+++ src/kube-hint.js
@@ -8,41 +8,36 @@ type lintRules = {
 }
 */
 
-function KubeHintResults () {
-  this.errors = []
-  this.warnings = []
-  this.suggestions = []
-  this.summary = []
-  return this
+class KubeHintResults {
+  errors /*: Array<Object> */ = []
+  warnings /*: Array<Object> */ = []
+  suggestions /*: Array<Object> */ = []
+  summary /*: Array<Object> */ = []
+
+  error = (docNumber /*: number */, key /*: string */, message /*: string */) => {
+    this.errors.push({ docNumber, key, message })
+  }
+
+  warn = (docNumber /*: number */, key /*: string */, message /*: string */) => {
+    this.warnings.push({ docNumber, key, message })
+  }
+
+  suggest = (docNumber /*: number */, key /*: string */, message /*: string */) => {
+    this.suggestions.push({ docNumber, key, message })
+  }
+
+  summarize = (docNumber /*: number */, data /*: Object */) => {
+    this.summary.push({ docNumber, ...data })
+  }
 }
 
-module.exports = class KubeHint {
+class KubeHint {
   defaultLintRules = {
-    version: '1.15.1'
+    version: '1.15.4'
   }
 
-  _error (results /*: KubeHintResults */, docNumber /*: number */, key /*: string */, message /*: string */) {
-    results.errors.push({ docNumber, key, message })
-  }
-
-  _warn (results /*: KubeHintResults */, docNumber /*: number */, key /*: string */, message /*: string */) {
-    results.warnings.push({ docNumber, key, message })
-  }
-
-  _suggest (results /*: KubeHintResults */, docNumber /*: number */, key /*: string */, message /*: string */) {
-    results.suggestions.push({ docNumber, key, message })
-  }
-
-  _summarize (results /*: KubeHintResults */, message /*: string */) {
-    results.summary.push({ message })
-  }
-
-  mergeLintResults (results /*: KubeHintResults */ = {}, existing /*: KubeHintResults */) {
-    existing.errors = existing.errors.concat(results.errors || [])
-    existing.warnings = existing.warnings.concat(results.warnings || [])
-    existing.suggestions = existing.suggestions.concat(results.suggestions || [])
-    existing.summary = existing.summary.concat(results.summary || [])
-    return existing
+  constructor (lintRules /*: Object|void */) {
+    if (lintRules) this.defaultLintRules = lintRules
   }
 
   lint (
@@ -56,11 +51,11 @@ module.exports = class KubeHint {
       throw new Error('Lint expects a lintRules object as its second argument')
     }
 
-    let results /*: KubeHintResults */ = new KubeHintResults()
+    const results /*: KubeHintResults */ = new KubeHintResults()
 
     for (let i = 0; i < docs.length; i++) {
       const doc = docs[i]
-      results = this.mergeLintResults(this.lintDocument(doc, rules), results)
+      this.lintDocument(doc, i, results, rules)
     }
 
     return results
@@ -69,58 +64,74 @@ module.exports = class KubeHint {
   lintDocument (
     doc /*: Object */,
     docNumber /*: number */,
+    results /*: KubeResults */ = new KubeHintResults(),
     rules /*: lintRules */ = this.defaultLintRules
   ) {
-    const results /*: KubeHintResults */ = new KubeHintResults()
-
-    const error = this._error.bind(undefined, results, docNumber)
-    // const warn = this._warn.bind(undefined, results, docNumber)
-    // const suggest = this._suggest.bind(undefined, results, docNumber)
-    const summarize = this._summarize.bind(undefined, results)
+    const { error } = results
 
     // Basic structure sanity check
     if (!doc || typeof doc !== 'object') error(null, 'Document is not an object!')
     else if (!doc.apiVersion || typeof doc.apiVersion !== 'string') error('apiVersion', 'apiVersion is invalid!')
     else if (!doc.kind || typeof doc.kind !== 'string') error('kind', 'kind is invalid!')
-
     // Do not continue if any fatal errors above have occurred
     if (results.errors.length > 0) return results
 
-    const documentLinter = this.documentLinters[doc.kind.toLowerCase()]
+    const kind = doc.kind.toLowerCase()
+    const documentLinter = this.documentLinters[kind]
+
     if (documentLinter) {
       if (documentLinter[doc.apiVersion]) {
-        this.mergeLintResults(documentLinter[doc.apiVersion](doc, docNumber), results)
-      } else if (documentLinter[doc.apiVersion].default) {
-        this.mergeLintResults(documentLinter[doc.apiVersion].default(doc, docNumber), results)
+        documentLinter[doc.apiVersion](doc, docNumber, results)
+      } else if (documentLinter.default) {
+        documentLinter.default(doc, docNumber, results)
       }
     } else process.stdout.write(`-> Warning! No linter defined for ${doc.apiVersion}/${doc.kind}\n`)
-
-    summarize('A summary of things!')
 
     return results
   }
 
   documentLinters = {
+    persistentvolumeclaim: {
+      default: (doc /*: Object */, docNumber /*: number */, results /*: KubeHintResults */) => {
+        const kind = doc.kind.toLowerCase()
+        results.summarize(docNumber, { kind, docNumber, name: doc.metadata.name })
+        return results
+      }
+    },
     deployment: {
-      'apps/v1': (doc /*: Object */, docNumber /*: number */) => {
-        return this.documentLinters.deployment.default(doc, docNumber)
+      'apps/v1': (doc /*: Object */, docNumber /*: number */, results /*: KubeHintResults */) => {
+        return this.documentLinters.deployment.default(doc, docNumber, results)
       },
-      default: (doc /*: Object */, docNumber /*: number */) => {
-        const results = new KubeHintResults()
-        const suggest = this._suggest.bind(undefined, results, docNumber)
-        const error = this._error.bind(undefined, results, docNumber)
-        const warn = this._warn.bind(undefined, results, docNumber)
+      default: (doc /*: Object */, docNumber /*: number */, results /*: KubeHintResults */) => {
+        const { suggest, error, warn, summarize } = results
+        const kind = doc.kind.toLowerCase()
+        const containers = []
 
-        if (doc.spec.replicas < 2) suggest('spec.replicas', 'One replica implies a single point of failure!')
-        if (doc.spec.template.spec.containers.length < 1) error('spec.template.spec.containers.length', 'No containers in this Deployment?')
+        if (doc.spec.replicas < 2) suggest(docNumber, 'spec.replicas', 'One replica implies a single point of failure!')
+        if (doc.spec.template.spec.containers.length < 1) error(docNumber, 'spec.template.spec.containers.length', 'No containers in this Deployment?')
 
         for (let i = 0; i < doc.spec.template.spec.containers.length; i++) {
           const container = doc.spec.template.spec.containers[i]
-          if (!container.resources) warn(`spec.template.spec.containers[${i}]`, 'No resource limits defined!')
+          containers.push({
+            image: container.image,
+            ports: container.ports
+          })
+          if (!container.resources) warn(docNumber, `spec.template.spec.containers[${i}]`, 'No resource limits defined!')
         }
+
+        summarize(docNumber, {
+          kind,
+          name: doc.metadata.name,
+          containers,
+          message: `${doc.spec.replicas} ${doc.spec.replicas > 1 ? 'replicas' : 'replica'} of "${doc.spec.template.spec.containers.map(c => c.image).join('", "')}"`
+        })
 
         return results
       }
     }
   }
+}
+
+module.exports = {
+  KubeHint, KubeHintResults
 }
